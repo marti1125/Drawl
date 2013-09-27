99f129c6337520047385d37f9e6b8f0bd80ce8ce
Aaron Druck <aldruck@gmail.com>
2013-09-27 09:10:52 -0700

    adding a counter

--- js/app.js
+++ js/app.js
@@ -1,10 +1,14 @@
 //Saving image to FFOS
 
+var count = 0; 
+
 function addDrawing() {
   
       //based on this:
       //http://stackoverflow.com/questions/17332071/trying-to-save-canvas-png-data-url-to-disk-with-html5-filesystem-but-when-i-ret
       //https://developer.mozilla.org/en-US/docs/WebAPI/Device_Storage#Add_a_file
+    
+      count++;
   
       var pictures = navigator.getDeviceStorage("pictures");
       // don't test on simulator, doesnt work.   and need to unplug the device to acutally test
@@ -18,7 +22,8 @@ function addDrawing() {
       }
 
       var photofile   = new Blob( [ asArray.buffer ], {type: "image/png"} );
-      var mainphoto = pictures.addNamed(photofile, "DCIM/MISC/my-drawings6.png");
+      var mainphoto = pictures.addNamed(photofile, "DCIM/MISC/my-drawings" + count + ".png");
+      alert(count);
       request.onsuccess = function () {
         var nameofimage = this.mainphoto;
         alert('File "' + nameofimage + '" successfully wrote on the pictures');
