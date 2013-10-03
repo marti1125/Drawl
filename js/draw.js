8077793b3186a91eda8b9af747524f8aa82798f9
Aaron Druck <aldruck@gmail.com>
2013-10-03 11:37:19 -0700

    back to the good format

--- js/draw.js
+++ js/draw.js
@@ -111,20 +111,71 @@
     };
 
     // create a function to pass touch events and coordinates to drawer
-    function draw(event){
-       // get the touch coordinates
-       var coors = {
-          x: event.targetTouches[0].pageX,
-          y: event.targetTouches[0].pageY
-       };
-       // pass the coordinates to the appropriate handler
-       drawer[event.type](coors);
+    function draw(event) {
+        var type = null;
+        // map mouse events to touch events
+        switch(event.type){
+            case "mousedown":
+                    event.touches = [];
+                    event.touches[0] = {
+                        pageX: event.pageX,
+                        pageY: event.pageY
+                    };
+                    type = "touchstart";
+            break;
+            case "mousemove":
+                    event.touches = [];
+                    event.touches[0] = {
+                        pageX: event.pageX,
+                        pageY: event.pageY
+                    };
+                    type = "touchmove";
+            break;
+            case "mouseup":
+                    event.touches = [];
+                    event.touches[0] = {
+                        pageX: event.pageX,
+                        pageY: event.pageY
+                    };
+                    type = "touchend";
+            break;
+        }
+
+        // touchend clear the touches[0], so we need to use changedTouches[0]
+        var coors;
+        if(event.type === "touchend") {
+            coors = {
+                x: event.changedTouches[0].pageX,
+                y: event.changedTouches[0].pageY
+            };
+        }
+        else {
+            // get the touch coordinates
+            coors = {
+                x: event.touches[0].pageX,
+                y: event.touches[0].pageY
+            };
+        }
+        type = type || event.type
+        // pass the coordinates to the appropriate handler
+        drawer[type](coors);
     }
 
+    // detect touch capabilities
+    var touchAvailable = ('createTouch' in document) || ('ontouchstart' in window);
+
     // attach the touchstart, touchmove, touchend event listeners.
-    canvas.addEventListener('touchstart',draw, false);
-    canvas.addEventListener('touchmove',draw, false);
-    canvas.addEventListener('touchend',draw, false);
+    if(touchAvailable){
+        canvas.addEventListener('touchstart', draw, false);
+        canvas.addEventListener('touchmove', draw, false);
+        canvas.addEventListener('touchend', draw, false);
+    }
+    // attach the mousedown, mousemove, mouseup event listeners.
+    else {
+        canvas.addEventListener('mousedown', draw, false);
+        canvas.addEventListener('mousemove', draw, false);
+        canvas.addEventListener('mouseup', draw, false);
+    }
 
     function redrawAll() {
 
@@ -173,6 +224,13 @@
     $("#btn-undo").bind('touchstart', function(){
         interval = setInterval(undoLast, 50);
     }).bind('touchend', function(){
+        clearInterval(interval);
+    });
+
+    //for mouse
+    $("#btn-undo").mousedown(function () {
+        interval = setInterval(undoLast, 50);
+    }).mouseup(function () {
         clearInterval(interval);
     });
 
